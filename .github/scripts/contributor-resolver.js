73c0998adc050b89f6fd01960c95d319ce35a563
Mohammadali Sefidi Esfahani <mohammadali.sefidi@gmail.com>
2025-08-25 19:07:23 +0330

    Update contributor-resolver.js

--- .github/scripts/contributor-resolver.js
+++ .github/scripts/contributor-resolver.js
@@ -22,7 +22,8 @@ async function detectAllContributors() {
 
 async function loadManualMappings() {
   try {
-    const mappingPath = path.join(process.cwd(), '.github', 'config', 'contributor-identities.json');
+    const mappingPath = path.join(process.cwd(), 'config', 'contributor-identities.json');
+    console.log(mappingPath)
     const content = await fs.readFile(mappingPath, 'utf8');
     const data = JSON.parse(content);
     
