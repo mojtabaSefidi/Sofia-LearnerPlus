08dc4af24a907471bfc9572403efea033e54751e
Mohammadali Sefidi Esfahani <mohammadali.sefidi@gmail.com>
2025-08-29 23:23:07 +0330

    Update LineRecRecommendationStrategy.cs

--- src/RelationalGit.Recommendation/Strategies/Spreading/LineRecRecommendationStrategy.cs
+++ src/RelationalGit.Recommendation/Strategies/Spreading/LineRecRecommendationStrategy.cs
@@ -7,7 +7,7 @@ namespace RelationalGit.Recommendation
     public class LineRecRecommendationStrategy : ScoreBasedRecommendationStrategy
     {
         public LineRecRecommendationStrategy(string knowledgeSaveReviewerReplacementType, ILogger logger, string pullRequestReviewerSelectionStrategy, bool? addOnlyToUnsafePullrequests, string recommenderOption, bool changePast, string simulationType)
-            : base(knowledgeSaveReviewerReplacementType, logger, pullRequestReviewerSelectionStrategy, addOnlyToUnsafePullrequests,recommenderOption, changePast, simulationType)
+            : base(knowledgeSaveReviewerReplacementType, logger, pullRequestReviewerSelectionStrategy, addOnlyToUnsafePullrequests, recommenderOption, changePast, simulationType)
         {
         }
 
